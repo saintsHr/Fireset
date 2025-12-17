c332e1c0bc588e1f9b7e8f416f384eab23dfa5f8
saintsHr <hiqkrs@gmail.com>
2025-12-17 16:02:13 -0300

    doc: core doc finished

--- include/fireset/core.h
+++ include/fireset/core.h
@@ -10,7 +10,19 @@
  * @{
  */
 
+/**
+ * @brief Initializes the engine, should be called on the very start of the program,
+ * before using the engine.
+ * 
+ * @retval true - success, engine initialized
+ * @retval false - failed, engine not initialized
+ */
 bool fsInit();
+
+/**
+ * @brief Shutdowns the engine, should be called on the end of the program, after using
+ * the engines
+ */
 void fsExit();
 
 /** @} */

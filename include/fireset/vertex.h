06ba5b1240fcf44ba7edd8c2ce1957bb180903fe
saintsHr <hiqkrs@gmail.com>
2025-12-17 16:37:33 -0300

    doc: vertex doc finished

--- include/fireset/vertex.h
+++ include/fireset/vertex.h
@@ -5,15 +5,25 @@
  * @{
  */
 
+/**
+ * @brief 2D Vector.
+ * 
+ * Used to define a position or velocity on a 2 dimentional space.
+ */
 typedef struct{
-    float x;
-    float y;
+    float x; /**< Horizontal axis. */
+    float y; /**< Vertical axis. */
 }FsVec2;
 
+/**
+ * @brief 3D Vector.
+ * 
+ * Used to define a position or velocity on a 3 dimentional space.
+ */
 typedef struct{
-    float x;
-    float y;
-    float z;
+    float x; /**< Horizontal axis. */
+    float y; /**< Vertical axis. */
+    float z; /**< Depth axis. */
 }FsVec3;
 
 /** @} */

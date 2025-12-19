194e444d0bbb87c06a3716627d650f8520dc188e
saintsHr <hiqkrs@gmail.com>
2025-12-19 15:55:50 -0300

    doc: time module documentation

--- include/fireset/fstime.h
+++ include/fireset/fstime.h
@@ -1,5 +1,22 @@
 #pragma once
 
+/**
+ * @brief Gets time.
+ * 
+ * Gets the real system time on the function call.
+*/
 double fsGetSystemTime(void);
+
+/**
+ * @brief Gets delta time.
+ * 
+ * Gets the delta time on the function call.
+*/
 double fsGetDeltaTime(void);
+
+/**
+ * @brief Inits time module.
+ * 
+ * Initializes the time module of the engine.
+*/
 void fsTimeInit(void);
